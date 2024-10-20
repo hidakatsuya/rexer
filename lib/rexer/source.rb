21f61b5354d5facc4d726727c899ebb7cfd65535
Katsuya Hidaka <hidakatsuya@gmail.com>
2024-10-21 01:03:51 +0900

    Modified Source loading and retriving to a simpler implementation

--- lib/rexer/source.rb
+++ lib/rexer/source.rb
@@ -1,13 +1,12 @@
 module Rexer
   module Source
-    def self.names = @names ||= []
+    TYPE = {
+      git: Git,
+      github: Github
+    }.freeze
 
     def self.from_definition(source)
-      const_get(source.type.capitalize).new(**source.options)
+      TYPE[source.type].new(**source.options)
     end
   end
 end
-
-require_relative "source/base"
-require_relative "source/git"
-require_relative "source/github"
